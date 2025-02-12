d5e569a6054aa29c2a4791f6f3b6167eb3dcd4a3
heitzjulien <heitzjulien@pm.me>
2025-02-12 21:34:42 +0100

    chore: remove ssr

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core";
 import { ButtonModule } from "primeng/button";
 import { CommonModule } from "@angular/common";
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 @Component({
   selector: "app-header",
-  imports: [ButtonModule, CommonModule],
+  imports: [ButtonModule, CommonModule, RouterLinkActive, RouterLink],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.css",
 })
