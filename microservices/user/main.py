50329d7e47244f2470947aef739e63826a3b85d2
mattisavec2t <mattis.almeidalima@gmail.com>
2025-02-13 11:18:56 +0100

    fix: db usage

--- microservices/user/main.py
+++ microservices/user/main.py
@@ -4,12 +4,12 @@ from shared.utils.database import Database
 
 app = FastAPI()
 
+db = Database()  # Instanciation de la classe Database
 
 @app.get("/")
 def read_root():
-    Database.query("SELECT * FROM user;")
-    return {"Hello": "World! user"}
-
+    result = db.query("SELECT * FROM user;")  # Utilisation de l'instance db
+    return {"Hello": "World!", "users": result}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
