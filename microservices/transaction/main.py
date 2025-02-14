21f2cc3346a973e1c15c6d910d6c4b9b40bfc815
mattisavec2t <mattis.almeidalima@gmail.com>
2025-02-14 12:24:58 +0100

    feat: add get_transactions function Il manque les vérifications de permission + gestion d'erreurs

--- microservices/transaction/main.py
+++ microservices/transaction/main.py
@@ -1,14 +1,38 @@
 from typing import Union
 from fastapi import FastAPI
+from shared.utils.database import Database
 
 app = FastAPI()
-
+db = Database()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World! transaction"}
+def get_transactions():
+    ### Route accessible uniquement si connecté
+    query = '''
+        SELECT
+            t.id, t.status, t.validated_at,
+            u.firstname, u.lastname,
+            v.id id_vehicle, v.model, v.price, v.type,
+            CASE
+                WHEN v.type = 'Location' THEN t.start_time
+                ELSE NULL
+            END AS start_time,
+            CASE
+                WHEN v.type = 'Location' THEN t.end_time
+                ELSE NULL
+            END AS end_time
+        FROM "transaction" t
+            JOIN "user" u ON t.id_user = u.id
+            JOIN "vehicle" v ON t.id_vehicle = v.id
+    '''
+    params = []
+    admin = True #Mettre une vrai fonction qui vérifie si on est admin
+    if not admin :
+        id_user = 1 #Récupérer le vrai id du client
+        query += ' WHERE u.id = %s'
+        params.append(id_user)
+    query += ' ORDER BY t.validated_at IS NOT NULL, t.validated_at DESC'
 
+    results = db.query(query, params)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    return {"data": results}
