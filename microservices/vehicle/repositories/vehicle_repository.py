5f96fa22439eeb2efc797df9822e63629bb8cec6
Kobrae-San <djedjegboble.pro@gmail.com>
2025-02-14 23:56:54 +0100

    feat: enhance vehicle creation with RETURNING clause and improve vehicle retrieval query

--- microservices/vehicle/repositories/vehicle_repository.py
+++ microservices/vehicle/repositories/vehicle_repository.py
@@ -6,9 +6,22 @@ class VehicleRepository:
         self.db = db
 
     def _create(self, vehicle_model = VehicleModel):
-        return self.db.query(f"INSERT INTO vehicle VALUES model, year, km, type, price, brand, energy, category, description", [vehicle_model.model, vehicle_model.year, vehicle_model.km, vehicle_model.type, vehicle_model.price, vehicle_model.brand, vehicle_model.energy, vehicle_model.category, vehicle_model.description])
+        return self.db.query(f"""
+                             INSERT INTO vehicle (model, year, km, type, price, brand, energy, category, description) 
+                             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                             RETURNING id, model, year, km, type, price, brand, energy, category, description
+                             """, 
+                             [
+                                 vehicle_model.model, 
+                                 vehicle_model.year, 
+                                 vehicle_model.km, 
+                                 vehicle_model.type, 
+                                 vehicle_model.price, 
+                                 vehicle_model.brand, 
+                                 vehicle_model.energy, 
+                                 vehicle_model.category, 
+                                 vehicle_model.description
+                            ])
     
     def get_vehicles(self):
-        print(self.db.query("SELECT * FROM vehicle"))
-        return self.db.query("SELECT * FROM vehicle")
-        # return "GodDamn !"
+        return self.db.query("SELECT id, model, year, km, type, price, brand, energy, category, description FROM vehicle")
