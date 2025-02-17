7c8c4d5a6346c5e8ead4cb5518dc18066e468770
Kobrae-San <djedjegboble.pro@gmail.com>
2025-02-17 18:07:27 +0100

    Refactor main application setup to streamline initialization and add error handling

--- microservices/vehicle/main.py
+++ microservices/vehicle/main.py
@@ -1,35 +1,10 @@
 from typing import Union
-from fastapi import FastAPI
-from routes.vehicle_router import VehicleRouter
-from controllers.vehicle_controller import VehicleController
-from services.vehicle_service import VehicleService
-from repositories.vehicle_repository import VehicleRepository
-from shared.utils.database import Database
+from routes.vehicle_router import vehicle_router
+from shared.utils.app_config import app_config
+from shared.utils.error_handler import add_error_handlers
 
+app = app_config()
 
-def create_app() -> FastAPI:
-    
-    db = Database()
+app.include_router(vehicle_router)
 
-    db._connect()
-    app = FastAPI()
-
-    vehicle_repository = VehicleRepository(db)
-    vehicle_service = VehicleService(vehicle_repository)
-    vehicle_controller = VehicleController(vehicle_service)
-    vehicle_router = VehicleRouter(vehicle_controller)
-
-    app.include_router(vehicle_router.get_router())
-    
-    return app
-
-app = create_app()
-
-# @app.get("/")
-# def read_root():
-#     return {"Hello": "World! vehicle"}
-
-
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
+add_error_handlers(app)
