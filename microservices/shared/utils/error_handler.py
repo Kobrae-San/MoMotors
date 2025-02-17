3039778f6b677f4af44a64b70490c075368f7aa8
mattisavec2t <mattis.almeidalima@gmail.com>
2025-02-17 09:48:07 +0100

    fix: response normalisation

--- microservices/shared/utils/error_handler.py
+++ microservices/shared/utils/error_handler.py
@@ -4,26 +4,27 @@ from fastapi.responses import JSONResponse
 async def not_found_handler(request: Request, exc: HTTPException):
     return JSONResponse(
         status_code=404,
-        content={"error": "Not found"}
+        content={"success": False, "message": "Not found"}
     )
 
 async def internal_error_handler(request: Request, exc: HTTPException):
     return JSONResponse(
         status_code=500,
-        content={"error": "Internal Server Error"}
+        content={"success": False, "message": "Internal Server Error"}
     )
 
 async def connection_error_handler(request: Request, exc: Exception):
     return JSONResponse(
         status_code=503,
-        content={"error": f"Database connection error: {exc}"}
+        content={"success": False, "message": f"Database connection error: {exc}"}
     )
 
 async def query_error_handler(request: Request, exc: Exception):
     return JSONResponse(
         status_code=400,
-        content={"error": f"Query execution error: {exc}"}
+        content={"success": False, "message": f"Query execution error: {exc}"}
     )
+
 
 # Fonction pour ajouter tous les gestionnaires d'erreurs à l'application FastAPI
 def add_error_handlers(app):
--- microservices/transaction/controller.py
+++ microservices/transaction/controller.py
@@ -1,4 +1,4 @@
 from repository import fetch_transactions
 
 def get_transactions():
-    return {"data": fetch_transactions()}
+    return {"success": True, "data": fetch_transactions()}
