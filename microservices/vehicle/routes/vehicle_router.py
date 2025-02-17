af1ed389fbcb386b78a236b8b9fe7253e3761a1e
Kobrae-San <djedjegboble.pro@gmail.com>
2025-02-17 18:07:42 +0100

    Refactor vehicle router to streamline route definitions and utilize controller functions directly

--- microservices/vehicle/routes/vehicle_router.py
+++ microservices/vehicle/routes/vehicle_router.py
@@ -1,31 +1,22 @@
 from fastapi import APIRouter
 from typing import List
 from shared.models.vehicle import VehicleModel
-from controllers.vehicle_controller import VehicleController
+from controllers.vehicle_controller import create_vehicle_controller, get_all_vehicles_controller, update_vehicle_controller, delete_vehicle_controller
 
-# vehicle_router = APIRouter()
+vehicle_router = APIRouter()
 
-# @vehicle_router.get("/vehicle/")
-# async def get_all_vehicle():
-#     return [{"Model" : "Yatout Yatout"}]
+@vehicle_router.get("/vehicle")
+def get_all_vehicles_route():
+    return get_all_vehicles_controller()
 
-# @vehicle_router.get("/vehicle/{item_id}")
-# async def get_vehicle_by_id(item_id: int):
-#     return [{"Model" : "F-150"}, {"Item_id" : f"{item_id}"}]
+@vehicle_router.post("/vehicle/create")
+def create_vehicle_route(vehicle_model: VehicleModel):
+    return create_vehicle_controller(vehicle_model)
 
-# @vehicle_router.get("/")
+@vehicle_router.put("/vehicle/update/{vehicle_id}")
+def update_vehicle_route(vehicle_id: int, vehicle_model: VehicleModel):
+    return update_vehicle_controller(vehicle_id, vehicle_model)
 
-
-class VehicleRouter:
-    def __init__(self, vehicle_controller = VehicleController):
-        self.router = APIRouter(prefix="/vehicle", tags=["vehicle"])
-        self.controller = vehicle_controller
-        self._setup_routes()
-
-    def _setup_routes(self):
-        self.router.get("/", response_model=List[VehicleModel])(self.controller.get_all_vehicles)
-        self.router.post("/create", response_model=VehicleModel)(self.controller.create_vehicle)
-        # self.router.put("/update", response_model=VehicleModel)(self.controller.update_vehicle)
-
-    def get_router(self) -> APIRouter:
-        return self.router
+@vehicle_router.delete("/vehicle/delete/{vehicle_id}")
+def delete_vehicle_route(vehicle_id: int):
+    return delete_vehicle_controller(vehicle_id)
