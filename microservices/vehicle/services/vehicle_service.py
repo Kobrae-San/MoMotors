cf13de8ef93b4aa605d4f5da2cf2124aaa06ecae
Kobrae-San <djedjegboble.pro@gmail.com>
2025-02-18 11:47:23 +0100

    Add service function to retrieve a vehicle by ID

--- microservices/vehicle/services/vehicle_service.py
+++ microservices/vehicle/services/vehicle_service.py
@@ -1,4 +1,4 @@
-from repositories.vehicle_repository import insert_vehicle, select_vehicles, update_vehicle_by_id, delete_vehicle_by_id
+from repositories.vehicle_repository import insert_vehicle, select_vehicles, select_vehicle_by_id, update_vehicle_by_id, delete_vehicle_by_id
 from repositories.user_repository import select_user_is_admin
 from shared.models.vehicle import VehicleModel
 from typing import List
@@ -11,6 +11,9 @@ def create_vehicle_service(user_id : int, vehicle_model: VehicleModel):
 
 def get_all_vehicles_service():
     return select_vehicles()
+
+def get_vehicle_by_id_service(vehicle_id: int):
+    return select_vehicle_by_id(vehicle_id)
 
 def update_vehicle_service(user_id: int, vehicle_id: int, vehicle_model = VehicleModel):
     is_user_admin = select_user_is_admin(user_id)
