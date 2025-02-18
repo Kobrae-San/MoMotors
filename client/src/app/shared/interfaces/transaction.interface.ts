1cb0cd1f3ee8ea9493baef957fdfaa95831398ee
heitzjulien <heitzjulien@pm.me>
2025-02-18 09:47:21 +0100

    fix: add optional field

--- client/src/app/shared/interfaces/transaction.interface.ts
+++ client/src/app/shared/interfaces/transaction.interface.ts
@@ -1,14 +1,14 @@
 import { TransactionStatus } from "../enums/transaction.enum";
 
 export default interface Transaction {
-  id?: number;
+  id: number;
   id_vehicle: number;
   id_user: number;
   status: TransactionStatus;
   id_admin: number;
-  created_at: Date;
-  updated_at: Date;
-  validated_at: Date;
-  start_time: Date;
-  end_time: Date;
+  created_at?: Date;
+  updated_at?: Date;
+  validated_at?: Date;
+  start_time?: Date;
+  end_time?: Date;
 }
