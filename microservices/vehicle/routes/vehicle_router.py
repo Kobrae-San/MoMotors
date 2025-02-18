575ee7c4b98c9d1f8beee3e0f6427671116b268a
Kobrae-San <djedjegboble.pro@gmail.com>
2025-02-18 11:47:18 +0100

    Add route for retrieving a vehicle by ID

--- microservices/vehicle/routes/vehicle_router.py
+++ microservices/vehicle/routes/vehicle_router.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter
 from typing import List
 from shared.models.vehicle import VehicleModel
-from controllers.vehicle_controller import create_vehicle_controller, get_all_vehicles_controller, update_vehicle_controller, delete_vehicle_controller
+from controllers.vehicle_controller import create_vehicle_controller, get_all_vehicles_controller, get_vehicle_by_id_controller, update_vehicle_controller, delete_vehicle_controller
 
 vehicle_router = APIRouter()
 
 @vehicle_router.get("/vehicle")
 def get_all_vehicles_route():
     return get_all_vehicles_controller()
+
+@vehicle_router.get("/vehicle/{vehicle_id}")
+def get_vehicle_by_id_route(vehicle_id: int):
+    return get_vehicle_by_id_controller(vehicle_id)
 
 @vehicle_router.post("/vehicle/create/{user_id}")
 def create_vehicle_route(user_id, vehicle_model: VehicleModel):
