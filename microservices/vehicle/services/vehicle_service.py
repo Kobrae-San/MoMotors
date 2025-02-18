99da681e0bcc31b7d03e9dbab94c2d967a7d4daf
Kobrae-San <djedjegboble.pro@gmail.com>
2025-02-18 11:33:46 +0100

    Uncomment vehicle service functions for full CRUD functionality

--- microservices/vehicle/services/vehicle_service.py
+++ microservices/vehicle/services/vehicle_service.py
@@ -3,23 +3,23 @@ from repositories.user_repository import select_user_is_admin
 from shared.models.vehicle import VehicleModel
 from typing import List
 
-# def create_vehicle_service(user_id : int, vehicle_model: VehicleModel):
-#     is_user_admin = select_user_is_admin(user_id)
-#     if is_user_admin[0]["is_admin"]:
-#         return insert_vehicle(vehicle_model)
-#     return { "message" : "Access Denied"}
+def create_vehicle_service(user_id : int, vehicle_model: VehicleModel):
+    is_user_admin = select_user_is_admin(user_id)
+    if is_user_admin[0]["is_admin"]:
+        return insert_vehicle(vehicle_model)
+    return { "message" : "Access Denied"}
 
 def get_all_vehicles_service():
     return select_vehicles()
 
-# def update_vehicle_service(user_id: int, vehicle_id: int, vehicle_model = VehicleModel):
-#     is_user_admin = select_user_is_admin(user_id)
-#     if is_user_admin[0]["is_admin"]:
-#         return update_vehicle_by_id(vehicle_id, vehicle_model)
-#     return { "message" : "Access Denied"}
+def update_vehicle_service(user_id: int, vehicle_id: int, vehicle_model = VehicleModel):
+    is_user_admin = select_user_is_admin(user_id)
+    if is_user_admin[0]["is_admin"]:
+        return update_vehicle_by_id(vehicle_id, vehicle_model)
+    return { "message" : "Access Denied"}
 
-# def delete_vehicle_service(user_id: int, vehicle_id: int):
-#     is_user_admin = select_user_is_admin(user_id)
-#     if is_user_admin[0]["is_admin"]:
-#         return delete_vehicle_by_id(vehicle_id)
-#     return { "message" : "Access Denied"}
+def delete_vehicle_service(user_id: int, vehicle_id: int):
+    is_user_admin = select_user_is_admin(user_id)
+    if is_user_admin[0]["is_admin"]:
+        return delete_vehicle_by_id(vehicle_id)
+    return { "message" : "Access Denied"}
