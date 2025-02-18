d99eb4b5f37812c8b97fb2e8b5966883e8add59a
Kobrae-San <djedjegboble.pro@gmail.com>
2025-02-18 11:10:35 +0100

    Comment out vehicle CRUD operations and related user admin checks

--- microservices/vehicle/routes/vehicle_router.py
+++ microservices/vehicle/routes/vehicle_router.py
@@ -9,14 +9,14 @@ vehicle_router = APIRouter()
 def get_all_vehicles_route():
     return get_all_vehicles_controller()
 
-@vehicle_router.post("/vehicle/create/{user_id}")
-def create_vehicle_route(user_id, vehicle_model: VehicleModel):
-    return create_vehicle_controller(user_id, vehicle_model)
+# @vehicle_router.post("/vehicle/create/{user_id}")
+# def create_vehicle_route(user_id, vehicle_model: VehicleModel):
+#     return create_vehicle_controller(user_id, vehicle_model)
 
-@vehicle_router.put("/vehicle/{vehicle_id}/update/{user_id}")
-def update_vehicle_route(user_id: int, vehicle_id: int, vehicle_model: VehicleModel):
-    return update_vehicle_controller(user_id, vehicle_id, vehicle_model)
+# @vehicle_router.put("/vehicle/{vehicle_id}/update/{user_id}")
+# def update_vehicle_route(user_id: int, vehicle_id: int, vehicle_model: VehicleModel):
+#     return update_vehicle_controller(user_id, vehicle_id, vehicle_model)
 
-@vehicle_router.delete("/vehicle/{vehicle_id}/delete/{user_id}")
-def delete_vehicle_route(user_id: int, vehicle_id: int):
-    return delete_vehicle_controller(user_id, vehicle_id)
+# @vehicle_router.delete("/vehicle/{vehicle_id}/delete/{user_id}")
+# def delete_vehicle_route(user_id: int, vehicle_id: int):
+#     return delete_vehicle_controller(user_id, vehicle_id)
