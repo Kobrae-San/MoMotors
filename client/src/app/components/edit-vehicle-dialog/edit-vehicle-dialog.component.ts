49dc6076e2a99bac6ff3402b5b04303d60da3294
Kobrae-San <djedjegboble.pro@gmail.com>
2025-02-20 10:32:27 +0100

    fix: refactor edit vehicle dialog component with improved form handling and validation

--- client/src/app/components/edit-vehicle-dialog/edit-vehicle-dialog.component.ts
+++ client/src/app/components/edit-vehicle-dialog/edit-vehicle-dialog.component.ts
@@ -1,78 +1,177 @@
-import { Component, EventEmitter, Input, Output, OnInit } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnInit,
+  inject,
+  Inject,
+} from "@angular/core";
 import {
   FormBuilder,
   FormGroup,
   Validators,
   FormsModule,
+  ReactiveFormsModule,
 } from "@angular/forms";
-import { ReactiveFormsModule } from "@angular/forms";
+
 import { DialogModule } from "primeng/dialog";
 import { ButtonModule } from "primeng/button";
 import { InputText } from "primeng/inputtext";
+import { DynamicDialogConfig, DynamicDialogRef } from "primeng/dynamicdialog";
+import { Select, SelectModule } from "primeng/select";
+import { Textarea, TextareaModule } from "primeng/textarea";
+import { NgIf } from "@angular/common";
+import { MessageService } from "primeng/api";
+import {
+  VehicleType,
+  VehicleBrand,
+  VehicleEnergy,
+  VehicleCategory,
+} from "../../shared/enums/vehicle.enum";
+import { VehicleService } from "../../shared/services/vehicle.service";
 
 @Component({
   selector: "app-edit-vehicle-dialog",
   imports: [
+    TextareaModule,
+    SelectModule,
     DialogModule,
     ButtonModule,
-    InputText,
     FormsModule,
     ReactiveFormsModule,
+    NgIf,
   ],
+  providers: [MessageService],
   templateUrl: "./edit-vehicle-dialog.component.html",
   styleUrls: ["./edit-vehicle-dialog.component.css"],
 })
 export class EditVehicleDialogComponent implements OnInit {
-  @Input() displayEditVehicleDialog: boolean = false;
-  @Input() vehicle: any = {};
-  @Output() displayEditVehicleDialogChange = new EventEmitter<boolean>();
-  @Output() vehicleUpdated = new EventEmitter<any>();
+  form: FormGroup;
+  public vehicleType: { name: string; value: string }[] = [];
+  public vehicleBrand: { name: string; value: string }[] = [];
+  public vehicleEnergy: { name: string; value: string }[] = [];
+  public vehicleCategory: { name: string; value: string }[] = [];
 
-  vehicleForm!: FormGroup;
+  private vehicleService = inject(VehicleService);
+  private messageService = inject(MessageService);
+  private ref = inject(DynamicDialogRef);
 
-  constructor(private fb: FormBuilder) {}
+  vehicleDetails: any;
 
-  ngOnInit() {
-    this.initForm();
-  }
+  public config = inject(DynamicDialogConfig);
+  private fb = inject(FormBuilder);
 
-  ngOnChanges() {
-    if (this.vehicle) {
-      this.updateForm();
-    }
-  }
-
-  initForm() {
-    this.vehicleForm = this.fb.group({
-      id: [{ value: "", disabled: true }],
-      brand: ["", Validators.required],
+  constructor() {
+    this.form = this.fb.group({
       model: ["", Validators.required],
-      price: [0, [Validators.required, Validators.min(1)]],
+      year: [
+        "",
+        [Validators.required, Validators.min(1908), Validators.max(2025)],
+      ],
+      km: [``, Validators.required],
+      type: ["", Validators.required],
+      price: ["", Validators.required],
+      brand: ["", Validators.required],
+      energy: ["", Validators.required],
+      category: ["", Validators.required],
+      description: ["", [Validators.required, Validators.maxLength(255)]],
     });
   }
 
-  updateForm() {
-    this.vehicleForm.patchValue({
-      id: this.vehicle.id,
-      brand: this.vehicle.brand,
-      model: this.vehicle.model,
-      price: this.vehicle.price,
+  ngOnInit(): void {
+    this.vehicleType = this.enumToSelectOptions(VehicleType);
+    this.vehicleBrand = this.enumToSelectOptions(VehicleBrand);
+    this.vehicleEnergy = this.enumToSelectOptions(VehicleEnergy);
+    this.vehicleCategory = this.enumToSelectOptions(VehicleCategory);
+    this.vehicleDetails = this.config.data;
+
+    this.form = this.fb.group({
+      model: [`${this.vehicleDetails.model}`, Validators.required],
+      year: [
+        `${this.vehicleDetails.year}`,
+        [Validators.required, Validators.min(1908), Validators.max(2025)],
+      ],
+      km: [`${this.vehicleDetails.km}`, Validators.required],
+      type: [
+        this.checkIfValueMatch(this.vehicleType, this.vehicleDetails.type),
+        Validators.required,
+      ],
+      price: [`${this.vehicleDetails.price}`, Validators.required],
+      brand: [
+        this.checkIfValueMatch(this.vehicleBrand, this.vehicleDetails.brand),
+        Validators.required,
+      ],
+      energy: [
+        this.checkIfValueMatch(this.vehicleEnergy, this.vehicleDetails.energy),
+        Validators.required,
+      ],
+      category: [
+        this.checkIfValueMatch(
+          this.vehicleCategory,
+          this.vehicleDetails.category
+        ),
+        Validators.required,
+      ],
+      description: [
+        `${this.vehicleDetails.description}`,
+        [Validators.required, Validators.maxLength(255)],
+      ],
     });
   }
 
-  saveChanges() {
-    if (this.vehicleForm.valid) {
-      const updatedVehicle = {
-        ...this.vehicle,
-        ...this.vehicleForm.getRawValue(),
+  private enumToSelectOptions(enumObj: any): { name: string; value: string }[] {
+    return Object.keys(enumObj)
+      .filter(key => isNaN(Number(key)))
+      .map(key => ({
+        name: enumObj[key],
+        value: enumObj[key],
+      }));
+  }
+
+  private checkIfValueMatch(
+    valueToCheck: { name: string; value: string }[],
+    valueToCompare: string
+  ) {
+    return valueToCheck.find(item => item.name == valueToCompare);
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      const dataToSend = {
+        model: this.form.value.model,
+        year: this.form.value.year,
+        km: this.form.value.km,
+        type: this.form.value.type.name,
+        price: this.form.value.price,
+        brand: this.form.value.brand.name,
+        energy: this.form.value.energy.name,
+        category: this.form.value.category.name,
+        description: this.form.value.description,
       };
-      this.vehicleUpdated.emit(updatedVehicle);
-      this.closeDialog();
-    }
-  }
 
-  closeDialog() {
-    this.displayEditVehicleDialog = false;
-    this.displayEditVehicleDialogChange.emit(this.displayEditVehicleDialog);
+      this.vehicleService
+        .updateVehicle(this.vehicleDetails.id, 1, dataToSend)
+        .subscribe(
+          () => {
+            this.messageService.add({
+              severity: "success",
+              summary: "Suppression",
+              detail: `Le véhicule a bien été modifié dans la base de donnée.`,
+              life: 3000,
+            });
+            this.ref.close();
+          },
+          error => {
+            this.messageService.add({
+              severity: "error",
+              summary: "Erreur",
+              detail:
+                "Une erreur est survenue lors de la modification du véhicule dans la base de donnée.",
+              life: 3000,
+            });
+          }
+        );
+    }
   }
 }
