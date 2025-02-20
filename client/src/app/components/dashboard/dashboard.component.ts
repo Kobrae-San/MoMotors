2a75839658efef036e30bcdd99167e417793f3d5
Kobrae-San <djedjegboble.pro@gmail.com>
2025-02-20 10:32:18 +0100

    fix: integrate vehicle addition and editing functionality in dashboard component

--- client/src/app/components/dashboard/dashboard.component.ts
+++ client/src/app/components/dashboard/dashboard.component.ts
@@ -1,34 +1,46 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { PanelModule } from "primeng/panel";
 import { TableModule } from "primeng/table";
 import { TagModule } from "primeng/tag";
 import { ButtonModule } from "primeng/button";
 import { TooltipModule } from "primeng/tooltip";
-import { DashboardService } from "../../shared/services/dashboard.service";
 import { EditVehicleDialogComponent } from "../edit-vehicle-dialog/edit-vehicle-dialog.component";
+import { DialogService, DynamicDialogRef } from "primeng/dynamicdialog";
+import { VehicleAddComponent } from "../vehicle-add/vehicle-add.component";
+import { VehicleService } from "../../shared/services/vehicle.service";
 
 @Component({
   selector: "app-dashboard",
-  imports: [
-    PanelModule,
-    TableModule,
-    TagModule,
-    ButtonModule,
-    TooltipModule,
-    EditVehicleDialogComponent,
-  ],
+  imports: [PanelModule, TableModule, TagModule, ButtonModule, TooltipModule],
+  providers: [DialogService],
   templateUrl: "./dashboard.component.html",
   styleUrl: "./dashboard.component.css",
 })
 export class DashboardComponent implements OnInit {
-  displayEditVehicleDialog: boolean = false;
   vehicleData!: any[];
   selectedVehicle: any = null;
 
-  constructor(private dashboardService: DashboardService) {}
+  public ref: DynamicDialogRef | undefined;
+
+  /** SERVICES */
+  private dialogService = inject(DialogService);
+  private vehicleService = inject(VehicleService);
+
+  constructor() {}
 
   ngOnInit() {
-    this.vehicleData = this.dashboardService.getvehicleData();
+    this.vehicleService.getAllVehicles().subscribe({
+      next: data => {
+        this.vehicleData = data.data;
+      },
+      error: err => console.error("Erreur lors du chargement :", err),
+    });
+  }
+
+  getVehicles() {
+    this.vehicleService.getAllVehicles().subscribe(vehicles => {
+      this.vehicleData = vehicles.data;
+    });
   }
 
   getType(type: string) {
@@ -43,17 +55,33 @@ export class DashboardComponent implements OnInit {
   }
 
   editVehicle(vehicle: any) {
-    this.selectedVehicle = { ...vehicle };
-    this.displayEditVehicleDialog = true;
+    this.ref = this.dialogService.open(EditVehicleDialogComponent, {
+      header: "Modifier le véhicule",
+      width: "50%",
+      contentStyle: { height: "1000px", overflow: "auto" },
+      closable: true,
+      data: vehicle,
+    });
+    this.ref.onClose.subscribe(() => {
+      this.getVehicles();
+    });
   }
 
-  updateVehicle(updatedVehicle: any) {
-    const index = this.vehicleData.findIndex(
-      vehiculeItem => vehiculeItem.id === updatedVehicle.id
-    );
-    if (index !== -1) {
-      this.vehicleData[index] = updatedVehicle;
-    }
-    this.displayEditVehicleDialog = false;
+  addVehicle() {
+    this.ref = this.dialogService.open(VehicleAddComponent, {
+      header: "Ajout d'un vehicule",
+      width: "50%",
+      contentStyle: { height: "1000px", overflow: "auto" },
+      closable: true,
+    });
+    this.ref.onClose.subscribe(() => {
+      this.getVehicles();
+    });
+  }
+
+  deleteVehicle(vehicle: any) {
+    this.vehicleService.deleteVehicle(vehicle.id, 1).subscribe(() => {
+      this.getVehicles();
+    });
   }
 }
