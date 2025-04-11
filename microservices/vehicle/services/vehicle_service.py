18b166e893809fa314c05361d10de8e08d459f44
Kobrae-San <djedjegboble.pro@gmail.com>
2025-04-11 21:43:50 +0200

    feat: implement async service for adding vehicle pictures with admin checks

--- microservices/vehicle/services/vehicle_service.py
+++ microservices/vehicle/services/vehicle_service.py
@@ -2,6 +2,8 @@ from repositories.vehicle_repository import insert_vehicle, select_vehicles, sel
 from repositories.user_repository import select_user_is_admin
 from shared.models.vehicle import VehicleModel
 from typing import List
+from fastapi import File, UploadFile
+from shared.utils.bucket import Bucket
 
 def create_vehicle_service(user_id : int, vehicle_model: VehicleModel):
     is_user_admin = select_user_is_admin(user_id)
@@ -11,6 +13,25 @@ def create_vehicle_service(user_id : int, vehicle_model: VehicleModel):
     else:
             return { "message" : "No such user"}
     return { "message" : "Access Denied"}
+
+async def add_vehicle_pictures_service(user_id: int, vehicle_id: int, files: List[UploadFile]):
+    is_user_admin = select_user_is_admin(user_id)
+    does_vehicle_exist = select_vehicle_by_id(vehicle_id)
+    if is_user_admin:
+        if is_user_admin[0]["is_admin"] and does_vehicle_exist:
+            uploaded_files = []
+            for file in files:
+                content = await file.read()
+                file_name = f"{file.filename}"
+                Bucket.write("vehicles/",content, file_name)
+                uploaded_files.append(file_name)
+            return {"uploaded": uploaded_files}
+        elif not does_vehicle_exist:
+            return { "message" : "No such vehicle"}
+    else:
+            return { "message" : "No such user"}
+    return { "message" : "Access Denied"}
+    
 
 def get_all_vehicles_service():
     return select_vehicles()
