f4dfed183efcf07cc1a4452a8ec70bfe0b3df1c6
Chanours <valentin.machefaux@gmail.com>
2025-04-11 13:16:32 +0200

    -add Verification du token et d'une protection des routes ajoutée

--- microservices/user/routes/user_router.py
+++ microservices/user/routes/user_router.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Depends
 from controllers.user_controller import create_user_controller, get_all_users_controller, login_controller
 from shared.models.auth import Auth, UserRegistration
+from shared.utils.auth_middleware import get_current_user
 
 user_router = APIRouter()
 
@@ -18,3 +19,8 @@ def login_route(request: Auth):
 @user_router.post("/register")
 def register_route(request: UserRegistration):
     return create_user_controller(request)
+
+@user_router.get("/me")
+def get_current_user_route(current_user = Depends(get_current_user)):
+    """Route pour obtenir les informations de l'utilisateur connecté"""
+    return {"success": True, "data": current_user}
